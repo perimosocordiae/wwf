130a4ec29c4ed1d37e1665a9891be53b28025bcf
CJ Carey <perimosocordiae@gmail.com>
2018-05-27 21:07:54 -0400

    Remove hardcoded board size in Cython code

--- _scorer.pyx
+++ _scorer.pyx
@@ -1,18 +1,19 @@
 # cython: boundscheck=False
 # cython: wraparound=False
 # cython: nonecheck=False
+from cython.view cimport array as cvarray
+
 __all__ = ['Scorer', 'all_words']
 
 cdef:
   int BINGO_BONUS = 35
-  int BOARD_SIZE = 15
   int* LETTER_VALUES = [
       1, 4, 4, 2, 1, 4, 3, 3, 1, 10, 5, 2, 4,  # A - M
       2, 1, 4, 10, 1, 1, 1, 2, 5, 4, 8, 3, 10  # N - Z
   ]
 
 cdef class Scorer:
-  cdef char[15][15] board
+  cdef char[:,::1] board
   cdef dict wordlist
   cdef int[27] hand_ct  # holds counts for each letter + wild in the hand
 
@@ -21,12 +22,15 @@ cdef class Scorer:
     cdef list row
     cdef bytes space, w
     cdef char* hand_ptr
+    cdef int board_size = len(board)
+    self.board = cvarray(shape=(board_size, board_size), itemsize=sizeof(char),
+                         format='c')
     # parse the board
-    for i in range(BOARD_SIZE):
+    for i in range(board_size):
       row = board[i]
-      for j in range(BOARD_SIZE):
+      for j in range(board_size):
         space = row[j]
-        self.board[i][j] = (<char*>space)[0]
+        self.board[i,j] = (<char*>space)[0]
     # group into subsets, by word length
     self.wordlist = dict()
     for w in wordlist:
@@ -74,13 +78,13 @@ cdef class Scorer:
     for (r,c) in play_loc:
       hword = _find_horiz(self.board,playdict,r,c)
       if (hword is not None and not _word_playable(hword.letters, hword.length,
-                                                         self.wordlist[hword.length],
-                                                         self.hand_ct)):
+                                                   self.wordlist[hword.length],
+                                                   self.hand_ct)):
         return False
       vword = _find_vert(self.board,playdict,r,c)
       if (vword is not None and not _word_playable(vword.letters, vword.length,
-                                                        self.wordlist[vword.length],
-                                                        self.hand_ct)):
+                                                   self.wordlist[vword.length],
+                                                   self.hand_ct)):
         return False
     return True
 
@@ -114,14 +118,14 @@ cdef class HorizWord:
   cdef int[15] c_idxs
   cdef int length
 
-  cdef int score(self, char[15][15] board):
+  cdef int score(self, char[:,::1] board):
     cdef char space
     cdef char* letters = self.letters
     cdef int base_val, i, r = self.r_idx, s = 0, mult = 1
     cdef int* c_idxs = self.c_idxs
     for i in range(self.length):
       base_val = letter_value(letters[i])
-      space = board[r][c_idxs[i]]
+      space = board[r, c_idxs[i]]
       s += base_val
       if space == '2':
         s += base_val
@@ -139,14 +143,14 @@ cdef class VertWord:
   cdef int c_idx
   cdef int length
 
-  cdef int score(self, char[15][15] board):
+  cdef int score(self, char[:,::1] board):
     cdef char space
     cdef char* letters = self.letters
     cdef int base_val, i, c = self.c_idx, s = 0, mult = 1
     cdef int* r_idxs = self.r_idxs
     for i in range(self.length):
       base_val = letter_value(letters[i])
-      space = board[r_idxs[i]][c]
+      space = board[r_idxs[i], c]
       s += base_val
       if space == '2':
         s += base_val
@@ -166,11 +170,12 @@ cdef inline int letter_value(char x):
   return LETTER_VALUES[x - 65]
 
 
-cdef HorizWord _find_horiz(char[15][15] board, dict playdict, int r, int c):
+cdef HorizWord _find_horiz(char[:,::1] board, dict playdict, int r, int c):
   cdef int ci = c
+  cdef int board_size = board.shape[0]
   cdef char space
   while ci > 0:
-    space = board[r][ci-1]
+    space = board[r, ci-1]
     if space >= 'A' and space <= 'Z':
       ci -= 1
     else:
@@ -180,8 +185,8 @@ cdef HorizWord _find_horiz(char[15][15] board, dict playdict, int r, int c):
   cdef HorizWord w = HorizWord()
   cdef int i = 0
   cdef bytes tmp
-  while ci < BOARD_SIZE:
-    space = board[r][ci]
+  while ci < board_size:
+    space = board[r, ci]
     if space >= 'A' and space <= 'Z':
       w.letters[i] = space
       w.c_idxs[i] = ci
@@ -200,11 +205,12 @@ cdef HorizWord _find_horiz(char[15][15] board, dict playdict, int r, int c):
     return w
 
 
-cdef VertWord _find_vert(char[15][15] board, dict playdict, int r, int c):
+cdef VertWord _find_vert(char[:,::1] board, dict playdict, int r, int c):
   cdef int ri = r
+  cdef int board_size = board.shape[0]
   cdef char space
   while ri > 0:
-    space = board[ri-1][c]
+    space = board[ri-1, c]
     if space >= 'A' and space <= 'Z':
       ri -= 1
     else:
@@ -214,8 +220,8 @@ cdef VertWord _find_vert(char[15][15] board, dict playdict, int r, int c):
   cdef VertWord w = VertWord()
   cdef int i = 0
   cdef bytes tmp
-  while ri < BOARD_SIZE:
-    space = board[ri][c]
+  while ri < board_size:
+    space = board[ri, c]
     if space >= 'A' and space <= 'Z':
       w.letters[i] = space
       w.r_idxs[i] = ri
@@ -235,15 +241,17 @@ cdef VertWord _find_vert(char[15][15] board, dict playdict, int r, int c):
 
 
 cpdef list all_words(list board, play):
-  cdef char[15][15] _board
+  cdef int board_size = len(board)
+  cdef char[:,::1] _board = cvarray(shape=(board_size, board_size),
+                                    itemsize=sizeof(char), format='c')
   cdef int i, j
   cdef list row
   cdef bytes space
-  for i in range(BOARD_SIZE):
+  for i in range(board_size):
     row = board[i]
-    for j in range(BOARD_SIZE):
+    for j in range(board_size):
       space = row[j]
-      _board[i][j] = (<char*>space)[0]
+      _board[i, j] = (<char*>space)[0]
 
   cdef HorizWord hword
   cdef VertWord vword
